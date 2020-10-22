034804530223ce4f8cbe8026bde87970c6167a03
sandinosanchez <sandino320@gmail.com>
2020-10-22 11:05:40 -0300

    fix for pr github

--- src/com/zebrunner/jenkins/pipeline/tools/scm/github/GitHub.groovy
+++ src/com/zebrunner/jenkins/pipeline/tools/scm/github/GitHub.groovy
@@ -21,7 +21,7 @@ class GitHub extends Scm {
         PR_ACTION("prAction", "\$.action"),
         PR_SHA("prSha", "\$.pull_request.head.sha"),
         PR_NUMBER("prNumber", "\$.number"),
-        PR_REPO("prRepo", "\$.pull_request.base.repo.name"),
+        PR_REPO("prRepo", "\$.pull_request.base.repo.full_name"),
         PR_SOURCE_BRANCH("prSourceBranch", "\$.pull_request.head.ref"),
         PR_TARGET_BRANCH("prTargetBranch", "\$.pull_request.base.ref"),
         PR_FILTER_REGEX("prFilterExpression", "^((opened|reopened)\\spull_request)*?\$"),
