08df74909726a4bea8aab77edb331c4639dd7302
sandinosanchez <sandino320@gmail.com>
2020-10-26 16:38:50 -0300

    one more before restart

--- src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
+++ src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
@@ -8,7 +8,7 @@ class Gitlab extends Scm {
     Gitlab(context) {
         super(context)
         this.prRefSpec = "+refs/merge-requests/*:refs/remotes/origin/merge-requests/pr/*"
-        this.branchSpec = "%s"
+        this.branchSpec = "origin/%s"
     }
 
     enum HookArgs {
