5b7956947ac4f8d1a8fa78d301a9c58dbd0c24b5
sandinosanchez <sandino320@gmail.com>
2020-10-26 13:47:05 -0300

    trying out another branchspec for gitlab

--- src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
+++ src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
@@ -8,7 +8,7 @@ class Gitlab extends Scm {
     Gitlab(context) {
         super(context)
         this.prRefSpec = "+refs/merge-requests/*:refs/remotes/merge-requests/pr/*"
-        this.branchSpec = "%s"
+        this.branchSpec = "refs/merge-requests/:$s/head"
     }
 
     enum HookArgs {
@@ -43,8 +43,8 @@ class Gitlab extends Scm {
 
     @Override
     protected String branchSpec() {
-        logger.info("gitlab\nprRefSpec: ${this.prRefSpec}\nbranchSpec: ${String.format(this.branchSpec, Configuration.get('pr_source_branch'))}")
-        return String.format(branchSpec, Configuration.get('pr_source_branch'))
+        logger.info("gitlab\nprRefSpec: ${this.prRefSpec}\nbranchSpec: ${String.format(this.branchSpec, Configuration.get('pr_sha'))}")
+        return String.format(branchSpec, Configuration.get('pr_sha'))
     }
     
     @Override
