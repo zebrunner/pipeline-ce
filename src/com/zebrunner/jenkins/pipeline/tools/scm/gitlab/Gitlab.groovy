7185e88dfd5cef392739a90caae7f140ea3539c0
sandinosanchez <sandino320@gmail.com>
2020-10-26 18:14:39 -0300

    another try with pr_number gitlab

--- src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
+++ src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
@@ -7,8 +7,8 @@ class Gitlab extends Scm {
 
     Gitlab(context) {
         super(context)
-        this.prRefSpec = "+refs/merge-requests/*:refs/remotes/origin/merge-requests/pr/*"
-        this.branchSpec = "%s"
+        this.prRefSpec = "+refs/merge-requests/*:refs/remotes/origin/merge-requests/*"
+        this.branchSpec = "origin/merge-requests/%s"
     }
 
     enum HookArgs {
@@ -43,8 +43,8 @@ class Gitlab extends Scm {
 
     @Override
     protected String branchSpec() {
-        logger.info("gitlab\nprRefSpec: ${this.prRefSpec}\nbranchSpec: ${String.format(this.branchSpec, Configuration.get('pr_source_branch'))}")
-        return String.format(branchSpec, Configuration.get('pr_source_branch'))
+        logger.info("gitlab\nprRefSpec: ${this.prRefSpec}\nbranchSpec: ${String.format(this.branchSpec, Configuration.get('pr_number'))}")
+        return String.format(branchSpec, Configuration.get('pr_number'))
     }
     
     @Override
