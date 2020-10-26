5f2d2d10193366fd389242dc722e0186348cd0ff
sandinosanchez <sandino320@gmail.com>
2020-10-26 10:59:04 -0300

    fixing typo for sonar analysis

--- src/com/zebrunner/jenkins/pipeline/integration/sonar/SonarClient.groovy
+++ src/com/zebrunner/jenkins/pipeline/integration/sonar/SonarClient.groovy
@@ -31,8 +31,7 @@ class SonarClient extends HttpClient {
         if (isPullRequest) {
             // goals needed to decorete pr with sonar analysis
 
-            def gitType = Configuration.get("scmtype")
-            logger.info("gitType: $gitType")
+            def gitType = Configuration.get("scmType")
             switch (gitType) {
                 case "github":
                     goals += " -Dsonar.pullrequest.provider=Github \
