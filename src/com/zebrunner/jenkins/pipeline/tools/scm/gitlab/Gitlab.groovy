d652dae7bd4ef3126da548be323a35cbd791e1d4
sandinosanchez <sandino320@gmail.com>
2020-10-26 16:21:45 -0300

    adding honorRefSpec to checkout operation

--- src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
+++ src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
@@ -8,7 +8,7 @@ class Gitlab extends Scm {
     Gitlab(context) {
         super(context)
         this.prRefSpec = "+refs/merge-requests/*:refs/remotes/merge-requests/pr/*"
-        this.branchSpec = "refs/remotes/origin/%s"
+        this.branchSpec = "%s"
     }
 
     enum HookArgs {
