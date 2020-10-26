dcd05ec6917d6cd0968ef28f3a2b7480a1717d2c
sandinosanchez <sandino320@gmail.com>
2020-10-26 13:47:58 -0300

    typo

--- src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
+++ src/com/zebrunner/jenkins/pipeline/tools/scm/gitlab/Gitlab.groovy
@@ -8,7 +8,7 @@ class Gitlab extends Scm {
     Gitlab(context) {
         super(context)
         this.prRefSpec = "+refs/merge-requests/*:refs/remotes/merge-requests/pr/*"
-        this.branchSpec = "refs/merge-requests/:$s/head"
+        this.branchSpec = "refs/merge-requests/:%s/head"
     }
 
     enum HookArgs {
