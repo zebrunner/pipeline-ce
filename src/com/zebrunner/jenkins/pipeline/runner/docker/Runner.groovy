703d23e121676b3b7bae2eb68c52a228576040ec
vdelendik <vadim.delendik@gmail.com>
2020-12-16 19:51:07 +0300

    added sonar goals

--- src/com/zebrunner/jenkins/pipeline/runner/docker/Runner.groovy
+++ src/com/zebrunner/jenkins/pipeline/runner/docker/Runner.groovy
@@ -67,7 +67,7 @@ class Runner extends AbstractRunner {
                     
                     // hotfix to buildTool initialization 
                     def buildTool = "gradle"
-                    Configuration.set("gradle_tasks", "clean build")
+                    Configuration.set("gradle_tasks", "clean build " + sc.getGoals(true) + " sonarqube")
                     
                     context.stage("${this.buildTool} build") {
                         switch (buildTool.toLowerCase()) {
