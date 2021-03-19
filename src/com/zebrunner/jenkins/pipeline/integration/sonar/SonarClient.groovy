73f62be06dddf30301689f5115990dda528469cc
vdelendik <vadim.delendik@gmail.com>
2021-03-19 20:39:44 +0300

    #142: added wildcard for junit report path

--- src/com/zebrunner/jenkins/pipeline/integration/sonar/SonarClient.groovy
+++ src/com/zebrunner/jenkins/pipeline/integration/sonar/SonarClient.groovy
@@ -61,7 +61,7 @@ class SonarClient extends HttpClient {
         // Determine at run-time if we use maven or gradle.
         def extraGoals = ""
         if (isMaven()) {
-            extraGoals = " sonar:sonar -Dsonar.junit.reportPaths=target/surefire-reports/junitreports"
+            extraGoals = " sonar:sonar -Dsonar.junit.reportPaths=**/target/surefire-reports/junitreports/*.xml"
         }
         // Gradle has higher priority!
         if (isGradle()) {
