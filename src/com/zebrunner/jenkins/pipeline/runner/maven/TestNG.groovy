6ac495c205ecd202d24442d0c29adc4e1bf46d7c
vdelendik <vadim.delendik@gmail.com>
2021-04-01 19:30:45 +0300

    removed -DskipTests from maven runners

--- src/com/zebrunner/jenkins/pipeline/runner/maven/TestNG.groovy
+++ src/com/zebrunner/jenkins/pipeline/runner/maven/TestNG.groovy
@@ -101,7 +101,7 @@ public class TestNG extends Runner {
             context.timestamps {
                 if (isValid) {
                     getScm().clonePush()
-                    compile("-U clean compile test -DskipTests")
+                    compile("-U clean compile test")
                 }
                 
                 clean()
