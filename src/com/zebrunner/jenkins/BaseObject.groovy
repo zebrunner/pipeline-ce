600f999401c6759b173e5af5eca7085633793089
vdelendik <vadim.delendik@gmail.com>
2021-06-18 11:48:42 +0300

    simplified org folder name detection

--- src/com/zebrunner/jenkins/BaseObject.groovy
+++ src/com/zebrunner/jenkins/BaseObject.groovy
@@ -142,7 +142,7 @@ public abstract class BaseObject {
         if (jobName.equals("RegisterRepository") || jobName.equals("launcher") || jobName.equals("qtest-updater") || jobName.equals("testrail-updater")) {
             //equals means just root folder, i.e. empty org name
             orgFolderName = ""
-        } else if (jobName.contains("RegisterRepository") || jobName.contains("launcher") || jobName.contains("qtest-updater") || jobName.contains("testrail-updater")) {
+        } else {
             // cut everything after latest slash:
             // qps/RegisterRepository -> qps
             // test/qps/RegisterRepository -> test/qps
@@ -150,24 +150,6 @@ public abstract class BaseObject {
             orgFolderName = jobName.substring(0, index)
         }
         
-/*        
-        if (nameCount == 1 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater") || jobName.contains("launcher") || jobName.contains("RegisterRepository"))) {
-            // testrail-updater - i.e. empty org name
-            orgFolderName = ""
-        } else if (nameCount == 2 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater") || jobName.contains("launcher") || jobName.contains("RegisterRepository"))) {
-            // stage/testrail-updater - i.e. stage
-            orgFolderName = Paths.get(jobName).getName(0).toString()
-        } else if (nameCount == 2) {
-            // carina-demo/API_Demo_Test - i.e. empty orgFolderName
-            orgFolderName = ""
-        } else if (nameCount == 3) { //TODO: need to test use-case with views!
-            // qaprosoft/carina-demo/API_Demo_Test - i.e. orgFolderName=qaprosoft
-            orgFolderName = Paths.get(jobName).getName(0).toString()
-        } else {
-            throw new RuntimeException("Invalid job organization structure: '${jobName}'!")
-        }
-*/        
-
         context.println "orgFolderName: ${orgFolderName}"
         return orgFolderName
     }
