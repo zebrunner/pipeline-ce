2b2bec33284328e9c79255b7f6920e9887b07026
vdelendik <vadim.delendik@gmail.com>
2022-03-18 11:04:37 +0300

    #236: be able to print *.dumpstream files content

--- src/com/zebrunner/jenkins/pipeline/runner/maven/TestNG.groovy
+++ src/com/zebrunner/jenkins/pipeline/runner/maven/TestNG.groovy
@@ -827,12 +827,13 @@ public class TestNG extends Runner {
     }
 
     protected void printDumpReports() {
-        // print "**/*.dump" file content into the log 
-        def files = context.findFiles(glob: '**/*.dump')
+        // print *.dump and *.dumpstream files content into the log. 
+        def files = context.findFiles(glob: '**/*.dump*')
         for (int i = 0; i < files.length; i++) {
             currentBuild.result = BuildResult.FAILURE //explicitly mark build as fail
             logger.error("Detected dump: " + files[i].path)
             logger.info(context.readFile(file: files[i].path))
+            logger.info("") //print empty line
         }
     }
     
