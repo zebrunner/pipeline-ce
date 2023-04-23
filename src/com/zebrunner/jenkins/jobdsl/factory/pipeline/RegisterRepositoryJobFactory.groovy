76b3d4aad91435799ad1a7bb1a34776065b0825a
vdelendik <vadim.delendik@gmail.com>
2023-04-23 14:33:02 +0200

    #300: declared main as default branch for RegisterRepository

--- src/com/zebrunner/jenkins/jobdsl/factory/pipeline/RegisterRepositoryJobFactory.groovy
+++ src/com/zebrunner/jenkins/jobdsl/factory/pipeline/RegisterRepositoryJobFactory.groovy
@@ -21,7 +21,7 @@ public class RegisterRepositoryJobFactory extends PipelineFactory {
             parameters {
                 configure addExtensibleChoice('scmType', "gc_GIT_TYPE", "Version control system type", "github")
                 configure stringParam('repoUrl', "https://github.com/zebrunner/carina-demo.git", 'Repository for scanning')
-                configure stringParam('branch', 'master', 'SCM repository branch to run against')
+                configure stringParam('branch', 'main', 'SCM repository branch to run against')
                 configure stringParam('scmUser', '', 'SCM user')
                 configure stringParam('scmToken', '', 'CSM token with read permissions')
                 configure addExtensibleChoice('pipelineLibrary', "gc_PIPELINE_LIBRARY", "Groovy JobDSL/Pipeline library, for example: https://github.com/zebrunner/pipeline-ce/releases", "Zebrunner-CE")
