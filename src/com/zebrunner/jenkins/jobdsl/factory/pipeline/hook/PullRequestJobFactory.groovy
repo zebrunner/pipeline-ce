e861674f1ee7f6b80537d8e73587d42ae6f46bb9
vdelendik <vadim.delendik@gmail.com>
2024-03-05 14:06:22 +0100

    #328: reverted regexpFilterText change for on pull request jobs

--- src/com/zebrunner/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactory.groovy
+++ src/com/zebrunner/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactory.groovy
@@ -100,7 +100,7 @@ public class PullRequestJobFactory extends PipelineFactory {
                             printContributedVariables(isLogLevelActive(Logger.LogLevel.DEBUG))
                             printPostContent(isLogLevelActive(Logger.LogLevel.DEBUG))
                             silentResponse(false)
-                            regexpFilterText(String.format(webHookArgs.prFilterText, this.repoUrl))
+                            regexpFilterText(String.format(webHookArgs.prFilterText, resolveUrl(this.repoUrl)))
                             regexpFilterExpression(String.format(webHookArgs.prFilterExpression, this.repoUrl))
                         }
                     }
