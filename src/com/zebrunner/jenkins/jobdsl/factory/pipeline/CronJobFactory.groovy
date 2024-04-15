10808aca81c6c5b6076623bf6efd39ff60461442
vdelendik <vadim.delendik@gmail.com>
2024-04-15 17:27:35 +0200

    one more fix

--- src/com/zebrunner/jenkins/jobdsl/factory/pipeline/CronJobFactory.groovy
+++ src/com/zebrunner/jenkins/jobdsl/factory/pipeline/CronJobFactory.groovy
@@ -43,7 +43,7 @@ public class CronJobFactory extends PipelineFactory {
                     
                     pipelineTriggers {
                         triggers {
-                            ParameterizedTimerTrigger {
+                            org.jenkinsci.plugins.parameterizedscheduler.ParameterizedTimerTrigger {
                                 spec("* * * * *%foo=bar\n*/1 * * * *%foo=boo")
                                 //parameterizedSpecification("* * * * *%foo=bar\n*/1 * * * *%foo=boo")
                             }
